0237e1fa35639a8162cafbacad1d345691de26fb
Temitope <temitope.ogungbesan.5@gmail.com>
2018-04-05 16:52:30 +0100

    Added checkbox

--- backend/views/branches/_form.php
+++ backend/views/branches/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\Companies;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\branches */
@@ -12,8 +14,12 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'companies_company_id')->textInput() ?>
-
+    
+    <?= $form->field($model,'companies_company_id')->dropDownList(
+        ArrayHelper::map(Companies::find()->all(),'company_id','comapny_name'),
+        ['prompt'=>'Select Company']
+    )
+     ?>
     <?= $form->field($model, 'branch_name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'branch_address')->textInput(['maxlength' => true]) ?>
